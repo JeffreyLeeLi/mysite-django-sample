a9d49f532f53e5a5327b959b5fe9c8ad33fa9139
Jeffrey Lee Li <Jeffrey.Lee.Li@qq.com>
2018-03-03 13:08:51 +0800

    comment url conf

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	# /polls/
 	path('', views.index, name='index'),
+
+	# /polls/1/
 	path('<int:question_id>/', views.detail, name = 'detail')
 ]
