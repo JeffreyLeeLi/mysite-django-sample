ba98a1b3c3cfd9e11f3909c848d201fb4e2accd6
Jeffrey Lee Li <Jeffrey.Lee.Li@qq.com>
2018-03-03 12:46:37 +0800

    get all questions

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
 
 def index(request):
-	return HttpResponse("hello there~")
+	all_questions = Question.objects.all()
+	response = ', '.join([q.question_text for q in all_questions])
+	return HttpResponse(response)
