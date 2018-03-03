26b5e851440368f16a960aba087d96cf46680734
Jeffrey Lee Li <Jeffrey.Lee.Li@qq.com>
2018-03-03 13:01:44 +0800

    render via template

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
@@ -7,7 +8,14 @@ from .models import Question
 
 def index(request):
 	all_questions = Question.objects.all()
-	response = ', '.join([q.question_text for q in all_questions])
+	template = loader.get_template('polls/index.html')
+
+	context = {
+		'all_questions': all_questions,
+	}
+
+	response = template.render(context, request)
+
 	return HttpResponse(response)
 
 def detail(request, question_id):
