c6f58046ae1a6689021aa54a3994bba587f0b903
Jeffrey Lee Li <Jeffrey.Lee.Li@qq.com>
2018-03-05 15:20:03 +0800

    tweak import

--- polls/admin.py
+++ polls/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
-from .models import Choice
+from .models import Question, Choice
 
 class ChoiceInline(admin.TabularInline):
 	model = Choice
