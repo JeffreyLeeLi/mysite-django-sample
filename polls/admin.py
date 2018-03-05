f78f9ffa56aaddbb309e784055f9a5e11c5a116c
Jeffrey Lee Li <Jeffrey.Lee.Li@qq.com>
2018-03-05 13:26:27 +0800

    add a search field

--- polls/admin.py
+++ polls/admin.py
@@ -10,6 +10,7 @@ class ChoiceInline(admin.TabularInline):
 	extra = 0
 
 class QuestionAdmin(admin.ModelAdmin):
+	search_fields = ['question_text']
 	list_filter = ['pub_date']
 	list_display = ['question_text', 'pub_date', 'was_published_recently']
 
